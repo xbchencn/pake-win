e68063e833bdee50eb1ddaf2fedf7e920b9cbdd4
xbchen <xbchen69@best-inc.com>
2023-08-07 16:39:22 +0800

    first comit

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import path from 'path';
+import appRootPath from 'app-root-path';
+import typescript from 'rollup-plugin-typescript2';
+import alias from '@rollup/plugin-alias';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
+
+import pakeCliDevPlugin from './plugins/pakeCliDevPlugin.js';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const devPlugins = !isProduction ? [pakeCliDevPlugin()] : [];
+
+export default {
+  input: isProduction ? 'bin/cli.ts' : 'bin/dev.ts',
+  output: {
+    file: isProduction ? 'dist/cli.js' : 'dist/dev.js',
+    format: 'es',
+    sourcemap: !isProduction,
+  },
+  watch: {
+    include: 'bin/**',
+    exclude: 'node_modules/**',
+  },
+  plugins: [
+    json(),
+    typescript({
+      tsconfig: 'tsconfig.json',
+      clean: true, // Clear cache
+    }),
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      preventAssignment: true,
+    }),
+    alias({
+      entries: [{ find: '@', replacement: path.join(appRootPath.path, 'bin') }],
+    }),
+    ...devPlugins,
+  ],
+};
